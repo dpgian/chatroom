6f7a388f0ad6dc483f6fb1a2a3ac18ce47254ee8
dpgian <dplgian@gmail.com>
2020-10-09 14:40:04 +0100

    Sidebar layout no functionalities

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBYUQWmwuEN7LhTJTIKKh1STO01msBnzD0",
+    authDomain: "chatroom-87299.firebaseapp.com",
+    databaseURL: "https://chatroom-87299.firebaseio.com",
+    projectId: "chatroom-87299",
+    storageBucket: "chatroom-87299.appspot.com",
+    messagingSenderId: "1075098450390",
+    appId: "1:1075098450390:web:07f51d16f847dcedc6cc77"
+  };
